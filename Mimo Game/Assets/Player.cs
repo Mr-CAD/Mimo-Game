7d87a3273ba77e96cefbfebf1f10f1c6bb02f89a
Mr-CAD <44128918+Mr-CAD@users.noreply.github.com>
2018-10-23 12:35:47 -0400

    working

---
+++ Mimo Game/Assets/Player.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        var rigidBody = GetComponent<Rigidbody2D>();
+        // Start coding here
+        if (Input.GetKey("right"))
+        {
+            rigidBody.velocity = new Vector2(5, 0);
+        }
+        if (Input.GetKey("left"))
+        {
+            rigidBody.velocity = new Vector2(-5, 0);
+        }
+        if (Input.GetKey("space"))
+        {
+            rigidBody.velocity = new Vector2(0, 10);
+        }
+    }
+}
