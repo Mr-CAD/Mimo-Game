e9148b24b66c12f2a563fe5eb3e288587cc72a1a
Mr-CAD <44128918+Mr-CAD@users.noreply.github.com>
2018-10-24 13:14:33 -0400

    done

--- Mimo Game/Assets/Player.cs
+++ Mimo Game/Assets/Player.cs
@@ -13,7 +13,7 @@ public class Player : MonoBehaviour
     void Update()
     {
         var rigidBody = GetComponent<Rigidbody2D>();
-        // Start coding here
+        var transform = GetComponent<Transform>();
         if (Input.GetKey("right"))
         {
             rigidBody.velocity = new Vector2(5, 0);
@@ -22,9 +22,20 @@ public class Player : MonoBehaviour
         {
             rigidBody.velocity = new Vector2(-5, 0);
         }
-        if (Input.GetKey("space"))
+        if (Input.GetKeyDown("space"))
         {
             rigidBody.velocity = new Vector2(0, 10);
+        }
+        if (transform.position.y < -6)
+        {
+            if (transform.position.x < 2)
+            {
+                transform.position = new Vector2(-5, 2);
+            }
+            else
+            {
+                transform.position = new Vector2(2, 2);
+            }
         }
     }
 }
